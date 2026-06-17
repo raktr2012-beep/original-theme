99e0dc89456f2a03c4d4177bffe86f85970202f5
raktr2012-beep <raktr2012@gmail.com>
2026-06-17 12:39:43 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+　<meta name="viewport"content="width=device-width,
+          initial-scale=1.0">
+  <title>はじめてゅのWordpressテーマ</title>
+<link rel="stylesheet"href="<?php echo get_stylesheet_uri(); ?>">
+
+</head>
+<body>
+<header>
+  <p>ここはヘッダーです</p>
+</header>
+<main>
+  <h1>はじめてゅのオリジナルテーマ制作</h1>
+  <p>このページは、自分で作ったindex.phpから
+    表示されています</p>
+<footer>
+  <p>ここはフッターです</p>
+</footer>
+</body>
+</html>
